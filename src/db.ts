2912e1f9e38c03e03f2485a5115c957c2f9350e9
mnishina <mitsunorinishina@icloud.com>
2025-02-23 13:58:41 +0900

    feat: Create base page.

---
+++ src/db.ts
@@ -0,0 +1,71 @@
+const meta = {
+  SITE_ORNER: "Mitsunori Nishina",
+  ORNER_JOB: "Web Designer / Creative Developer",
+};
+
+const projects = [
+  {
+    projectYear: "2025",
+    projectName: "RAMEN2024",
+    projectURL: "https://ramen-2024.vercel.app/",
+  },
+  {
+    projectYear: "2024",
+    projectName: "RAMEN2023",
+    projectURL: "https://ramen-2023.vercel.app/",
+  },
+];
+
+const awards = [
+  {
+    year: "2025",
+    project: "RAMEN2024",
+    name: "Special Kudos",
+    at: "CSS Design Awards",
+    url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+  },
+  {
+    year: "2025",
+    project: "RAMEN2024",
+    name: "Best UI",
+    at: "CSS Design Awards",
+    url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+  },
+  {
+    year: "2025",
+    project: "RAMEN2024",
+    name: "Best UX",
+    at: "CSS Design Awards",
+    url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+  },
+  {
+    year: "2025",
+    project: "RAMEN2024",
+    name: "Best Innovation",
+    at: "CSS Design Awards",
+    url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+  },
+  {
+    year: "2025",
+    project: "RAMEN2024",
+    name: "STAR",
+    at: "CSS Winner",
+    url: "https://www.csswinner.com/details/ramen2024/18512",
+  },
+  {
+    year: "2024",
+    project: "RAMEN2023",
+    name: "STAR",
+    at: "CSS Winner",
+    url: "https://www.csswinner.com/details/ramen2023/17776",
+  },
+  {
+    year: "2024",
+    project: "RAMEN2023",
+    name: "Nominee",
+    at: "Awwwards",
+    url: "https://www.awwwards.com/sites/ramen2023",
+  },
+];
+
+export { meta, projects, awards };
