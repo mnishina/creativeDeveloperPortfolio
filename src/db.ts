81df720a29b63a9c3eed0b0b9d99f0e2fdc120ee
mnishina <mitsunorinishina@icloud.com>
2025-02-24 12:55:53 +0900

    feat: いったん画像追加と設置

--- src/db.ts
+++ src/db.ts
@@ -9,16 +9,19 @@ const projects = [
     projectYear: "2025",
     projectName: meta.SITE_NAME,
     projectURL: "https://cdpf.vercel.app/",
+    projectImage: "/projects_portfoliov01.avif",
   },
   {
     projectYear: "2025",
     projectName: "RAMEN2024",
     projectURL: "https://ramen-2024.vercel.app/",
+    projectImage: "/projects_ramen2024.avif",
   },
   {
     projectYear: "2024",
     projectName: "RAMEN2023",
     projectURL: "https://ramen-2023.vercel.app/",
+    projectImage: "/projects_ramen2023.avif",
   },
 ];
 
@@ -29,6 +32,7 @@ const awards = [
     name: "Special Kudos",
     at: "CSS Design Awards",
     url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+    image: "/specialKudos_ramen2024.avif",
   },
   {
     year: "2025",
@@ -36,6 +40,7 @@ const awards = [
     name: "Best UI",
     at: "CSS Design Awards",
     url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+    image: "/bestUI_ramen2024.avif",
   },
   {
     year: "2025",
@@ -43,6 +48,7 @@ const awards = [
     name: "Best UX",
     at: "CSS Design Awards",
     url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+    image: "/bestUX_ramen2024.avif",
   },
   {
     year: "2025",
@@ -50,6 +56,7 @@ const awards = [
     name: "Best Innovation",
     at: "CSS Design Awards",
     url: "https://www.cssdesignawards.com/sites/ramen2024/47087/",
+    image: "/bestInnovation_ramen2024.avif",
   },
   {
     year: "2025",
@@ -57,6 +64,7 @@ const awards = [
     name: "STAR",
     at: "CSS Winner",
     url: "https://www.csswinner.com/details/ramen2024/18512",
+    image: "/bestInnovation_ramen2024.avif",
   },
   {
     year: "2024",
@@ -64,6 +72,7 @@ const awards = [
     name: "STAR",
     at: "CSS Winner",
     url: "https://www.csswinner.com/details/ramen2023/17776",
+    image: "/bestInnovation_ramen2024.avif",
   },
   {
     year: "2024",
@@ -71,6 +80,7 @@ const awards = [
     name: "Nominee",
     at: "Awwwards",
     url: "https://www.awwwards.com/sites/ramen2023",
+    image: "/bestInnovation_ramen2024.avif",
   },
 ];
 
