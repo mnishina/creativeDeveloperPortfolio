d0b7bb71ea563db3ed62e2419ea5eaab15dffe60
mnishina <mitsunorinishina@icloud.com>
2025-03-13 23:09:58 +0900

    feat: uAlphaをfragmentShaderに設定

--- src/scripts/shader/fragmentShader.glsl
+++ src/scripts/shader/fragmentShader.glsl
@@ -1,9 +1,10 @@
 varying vec2 vUv;
 
 uniform sampler2D uTexture;
+uniform float uAlpha;
 
 void main() {
   vec4 textureColor = texture2D(uTexture, vUv);
   
-  gl_FragColor = textureColor;
+  gl_FragColor = vec4(textureColor.rgb, uAlpha);
 }
