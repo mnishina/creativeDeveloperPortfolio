41d5c573df9c1d6871c3a732608eac8497dc774a
mnishina <mitsunorinishina@icloud.com>
2025-03-26 00:28:06 +0900

    feat: managerの設定と処理の改良

--- src/scripts/common/loader.ts
+++ src/scripts/common/loader.ts
@@ -14,35 +14,41 @@ const loader: Loader = {
 function init() {}
 
 async function loadImages() {
-  const promises = [...app.$images].map(($image) => {
+  // const promises = [...app.$images].map(($image) => {
+  //   const imagePath = $image.getAttribute("src");
+
+  //   if (imagePath) {
+  //     return new Promise((resolve) => {
+  //       const texture = new TextureLoader(loader.loadingManager);
+  //       texture.load(imagePath, (loadedTexture) => {
+  //         loader.imageCache.set(imagePath, loadedTexture);
+
+  //         console.log("1");
+  //         resolve(loadedTexture);
+  //       });
+  //     });
+  //   }
+  // });
+
+  _manager();
+
+  app.$images.forEach(($image) => {
     const imagePath = $image.getAttribute("src");
 
+    const texture = new TextureLoader(loader.loadingManager);
     if (imagePath) {
-      return new Promise((resolve) => {
-        const texture = new TextureLoader(loader.loadingManager);
-        texture.load(imagePath, (loadedTexture) => {
-          // console.log("1");
-          loader.imageCache.set(imagePath, loadedTexture);
-
-          resolve(loadedTexture);
-        });
+      texture.load(imagePath, (loadedTexture) => {
+        loader.imageCache.set(imagePath, loadedTexture);
       });
     }
   });
-
-  loader.loadingManager.onProgress = (url, loaded, total) => {
-    _onProgress(url, loaded, total);
-  };
-  loader.loadingManager.onLoad = () => {
-    _onLoad();
-  };
-
-  return Promise.all(promises).then(() => {
-    console.log("2");
-    // console.log(loader.imageCache);
-  });
 }
 
+function _manager() {
+  loader.loadingManager.onProgress = (url, loaded, total) =>
+    _onProgress(url, loaded, total);
+  loader.loadingManager.onLoad = () => _onLoad();
+}
 function _onProgress(url: string, loaded: number, total: number) {
   console.log(url, loaded, total);
 }
